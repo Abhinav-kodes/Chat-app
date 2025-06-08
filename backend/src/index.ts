96c0954ea640147af5568c1f6a0802a706c0ef9e
Abhinav-kodes <abhinavsingh717073@gmail.com>
2025-06-09 01:37:42 +0530

    Routes setup

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route";
+import { connectDB } from "./lib/db"
+dotenv.config();
+
+const PORT = process.env.PORT
+const app = express();
+
+app.use("/api/auth", authRoutes)
+
+
+
+app.listen(PORT,() => {
+    console.log("Server is running on PORT:" + PORT);
+    connectDB()
+});
