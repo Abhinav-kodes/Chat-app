63fda6635481693ed0fe90aef50bee6589f60812
Abhinav-kodes <abhinavsingh717073@gmail.com>
2025-06-12 14:56:40 +0530

    login / logout completed

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,128 @@
-
+import { Link } from "react-router-dom";
+import "../App.css"
+import AuthImagePattern from "../components/AuthImagePattern";
+import { useAuthStore } from "../store/useAuthStore";
+import { Eye, EyeOff, Loader2, MessageSquare } from "lucide-react";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+    });
+
+    const { login, isLoggingIn } = useAuthStore();
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login(formData);
+  };
+
   return (
-    <div>LoginPage</div>
+     <div className="min-h-screen grid lg:grid-cols-2">
+      <div className="flex flex-col justify-center items-start p-6 ">
+        <div className="w-full max-w-md space-y-8 ml-50 bg-gray-300 rounded-xl p-6">
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div className="size-12 rounded-xl bf=g-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                <MessageSquare className="siz-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
+              <p className="text-base-content/60">Sign in to your account</p>
+            </div>
+          </div>
+          <form onSubmit={handleSubmit} className="space-y-6">
+
+            <label className="label">
+                <span className="label-text font-medium">Email</span>
+            </label>  
+            <div className="flex">
+              <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                  <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
+                  <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
+                </svg>
+              </span>
+              <input 
+                type="email" 
+                className={`rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} 
+                placeholder="you@example.com"
+                value={formData.email}
+                onChange={(e) => setFormData({ ...formData, email: e.target.value})} 
+                />
+            </div>
+
+            <label className="label">
+                <span className="label-text font-medium">Password</span>
+            </label>  
+            <div className="flex w-full">
+              <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+
+              <input
+                type={showPassword ? "text" : "password"}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-none block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                placeholder="•••••••••"
+                value={formData.password}
+                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+              />
+
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-s-0 border-gray-300 rounded-e-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+              >
+                {showPassword ? (
+                  <EyeOff className="size-5 text-base-content/40" />
+                ) : (
+                  <Eye className="size-5 text-base-content/40" />
+                )}
+              </button>
+            </div>
+              <button 
+              type="submit" 
+              className="text-white bg-purple-500 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-3 text-center dark:bg-purple-500 dark:hover:bg-purple-500  dark:focus:ring-purple-300" 
+              disabled={isLoggingIn}>
+              {isLoggingIn ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Login"
+              )}
+              </button>
+          </form>
+
+          <div className="text-center">
+            <p className="text-base-content/60">
+            Do not have an account?{" "}
+            <Link to="/signup" className="link link-primary">
+            register
+            </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+        />
+    </div>
   )
 }
 
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -12,6 +12,8 @@ interface AuthStoreState {
   isCheckingAuth: boolean;
   checkAuth: () => Promise<void>;
   signup: (data: { fullName: string; email: string; password: string }) => Promise<void>;
+  logout: () => Promise<void>;
+  login: (data: {email: string; password: string }) => Promise<void>;
 }
 
 export const useAuthStore = create<AuthStoreState>((set) => ({
@@ -50,10 +52,27 @@ export const useAuthStore = create<AuthStoreState>((set) => ({
             set({ isSigningUp: false });
         }
     },
+    
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data})
+            toast.success("Logged in successfully");
+        } catch (error: unknown) {
+            if (error instanceof AxiosError && error.response?.data?.message) {
+            toast.error(error.response.data.message);
+            } else {
+            toast.error("Something went wrong");
+            }
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
 
     logout: async () => {
         try {
-            await axiosInstance.post("/auth/logout");
+            await axiosInstance.get("/auth/logout");
             set({authUser: null });
             toast.success("Logged out successfully");
         } catch (error) {
@@ -63,5 +82,6 @@ export const useAuthStore = create<AuthStoreState>((set) => ({
                 toast.error("Something went wrong");
                 }        
         }
-    }
+    },
+
 }));
