e99b86e829206b9059c930bd6c6a59576b324404
Abhinav-kodes <abhinavsingh717073@gmail.com>
2025-06-17 23:56:00 +0530

    added th

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,12 +4,12 @@ import "../App.css"
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 const Navbara = () => {
-   const { logout, authUser } = useAuthStore();
+const { logout, authUser } = useAuthStore();
   
   return (
     // Use the imported components directly (e.g., <NavbarBrand> instead of <Navbar.Brand>)
    <header
-    className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+    className="border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
