b0813fa6e2f36cb19caaa57fb755c4bec13ee0e0
Abhinav-kodes <abhinavsingh717073@gmail.com>
2025-07-13 22:40:00 +0530

    themes

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -21,10 +21,10 @@ const LoginPage = () => {
   return (
      <div className="min-h-screen grid lg:grid-cols-2">
       <div className="flex flex-col justify-center items-start p-6 ">
-        <div className="w-full max-w-md space-y-8 ml-50 bg-gray-300 rounded-xl p-6">
+        <div className="w-full max-w-md space-y-8 ml-50 rounded-xl p-6">
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
-              <div className="size-12 rounded-xl bf=g-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
                 <MessageSquare className="siz-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -22,7 +22,7 @@ const ProfilePage = () => {
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
-        <div className="bg-gray-300 rounded-xl p-6 space-y-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
             <h1 className="text-2xl font-semibold">Profile</h1>
             <p className="mt-2">Your profile information</p>
@@ -40,11 +40,11 @@ const ProfilePage = () => {
               <label
               htmlFor="avatar-upload"
               className={`
-                absolute bottom-0 right-0 bg-gray-200 hover:scale-105 p-2 rounded-full cursor-pointer transition-all duration-200
+                absolute bottom-0 right-0 bg-base-content hover:scale-105 p-2 rounded-full cursor-pointer transition-all duration-200
                 ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
                 `}
               >
-                <Camera className="w-5 h-5 text-black-200" />
+                <Camera className="w-5 h-5 text-base-200" />
                 <input
                   type="file"
                   id="avatar-upload"
@@ -66,17 +66,17 @@ const ProfilePage = () => {
                   Full Name
                 </User>
               </div>
-              <p className="px-4 py-2.5 bg-gray-200 rounded-lg border">{authUser?.fullName}</p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.fullName}</p>
             </div>
             <div className="space-y-1.5">
               <div className="text-sm text-zinc-400 flex items-center gap-2">
                 <Mail className="w-4 h-4" />
                 Email address
               </div>
-              <p className="px-4 py-2.5 bg-gray-200 rounded-lg border">{authUser?.email}</p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
             </div>
           </div>
-          <div className="mt-6 bg-gray-300 rounded-xl p-6">
+          <div className="mt-6 bg-base-300 rounded-xl p-6">
             <h2 className="text-lg font-medium mb-4">Account Information</h2>
             <div className="space-y-3 text-sm">
               <div className="flex items-center justify-between py-2 border-b border-zinc-700">
